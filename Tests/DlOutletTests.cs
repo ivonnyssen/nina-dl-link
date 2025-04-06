e1e0004433813d2c7e15560d490257be479a3f51
Igor von Nyssen <igor@vonnyssen.com>
2025-04-06 09:12:45 -0700

    Addedf namespace for tests

--- Tests/DlOutletTests.cs
+++ Tests/DlOutletTests.cs
@@ -4,231 +4,234 @@ using Moq.Protected;
 using NINA.Equipment.Interfaces;
 using System.Net;
 
-public class DlOutletTests {
+namespace IgorVonNyssen.NINA.DlLink.Tests {
 
-    [Fact]
-    public async Task Poll_ShouldReturnTrue_WhenResponseIsSuccessful() {
-        // Arrange
-        var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
-        handlerMock
-            .Protected()
-            .Setup<Task<HttpResponseMessage>>(
+    public class DlOutletTests {
+
+        [Fact]
+        public async Task Poll_ShouldReturnTrue_WhenResponseIsSuccessful() {
+            // Arrange
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync(new HttpResponseMessage {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent("true")
+                })
+                .Verifiable();
+
+            var httpClient = new HttpClient(handlerMock.Object);
+
+            var dlOutlet = new DlOutlet("TestOutlet", 1, httpClient, "localhost", "user", "password");
+
+            // Act
+            var result = await dlOutlet.Poll();
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(1d, dlOutlet.Value);
+        }
+
+        [Fact]
+        public async Task Poll_ShouldReturnFalse_WhenResponseIsUnsuccessful() {
+            // Arrange
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync(new HttpResponseMessage {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Content = new StringContent("Bad Request")
+                })
+                .Verifiable();
+
+            var httpClient = new HttpClient(handlerMock.Object);
+
+            var dlOutlet = new DlOutlet("TestOutlet", 1, httpClient, "localhost", "user", "password");
+
+            // Act
+            var result = await dlOutlet.Poll();
+
+            // Assert
+            Assert.False(result);
+            Assert.Equal(0d, dlOutlet.Value);
+        }
+
+        [Fact]
+        public async Task Poll_ShouldReturnFalse_WhenHttpRequestExceptionOccurs() {
+            // Arrange
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ThrowsAsync(new HttpRequestException("Request failed"))
+                .Verifiable();
+
+            var httpClient = new HttpClient(handlerMock.Object);
+
+            var dlOutlet = new DlOutlet("TestOutlet", 1, httpClient, "localhost", "user", "password");
+
+            // Act
+            var result = await dlOutlet.Poll();
+
+            // Assert
+            Assert.False(result);
+            Assert.Equal(0d, dlOutlet.Value);
+        }
+
+        [Fact]
+        public async Task Poll_ShouldReturnFalse_WhenJsonExceptionOccurs() {
+            // Arrange
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync(new HttpResponseMessage {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent("invalid json")
+                })
+                .Verifiable();
+
+            var httpClient = new HttpClient(handlerMock.Object);
+
+            var dlOutlet = new DlOutlet("TestOutlet", 1, httpClient, "localhost", "user", "password");
+
+            // Act
+            var result = await dlOutlet.Poll();
+
+            // Assert
+            Assert.False(result);
+            Assert.Equal(0d, dlOutlet.Value);
+        }
+
+        [Fact]
+        public async Task SetValue_ShouldSetCorrectValue_WhenResponseIsSuccessful() {
+            // Arrange
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync(new HttpResponseMessage {
+                    StatusCode = HttpStatusCode.NoContent
+                })
+                .Verifiable();
+
+            var httpClient = new HttpClient(handlerMock.Object);
+
+            var dlOutlet = new DlOutlet("TestOutlet", 1, httpClient, "localhost", "user", "password") {
+                TargetValue = 1d
+            };
+
+            // Act
+            await ((IWritableSwitch)dlOutlet).SetValue();
+
+            // Assert
+            handlerMock.Protected().Verify(
                 "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
+                Times.Once(),
+                ItExpr.Is<HttpRequestMessage>(req =>
+                    req.Method == HttpMethod.Put &&
+                    req.RequestUri == new Uri("http://localhost/restapi/relay/outlets/1/state/") &&
+                    req.Content.ReadAsStringAsync().Result == "value=true"
+                ),
                 ItExpr.IsAny<CancellationToken>()
-            )
-            .ReturnsAsync(new HttpResponseMessage {
-                StatusCode = HttpStatusCode.OK,
-                Content = new StringContent("true")
-            })
-            .Verifiable();
+            );
+        }
 
-        var httpClient = new HttpClient(handlerMock.Object);
+        [Fact]
+        public async Task SetValue_ShouldLogError_WhenResponseIsUnsuccessful() {
+            // Arrange
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync(new HttpResponseMessage {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Content = new StringContent("Bad Request")
+                })
+                .Verifiable();
 
-        var dlOutlet = new DlOutlet("TestOutlet", 1, httpClient, "localhost", "user", "password");
+            var httpClient = new HttpClient(handlerMock.Object);
 
-        // Act
-        var result = await dlOutlet.Poll();
+            var dlOutlet = new DlOutlet("TestOutlet", 1, httpClient, "localhost", "user", "password") {
+                TargetValue = 1d
+            };
 
-        // Assert
-        Assert.True(result);
-        Assert.Equal(1d, dlOutlet.Value);
-    }
+            // Act
+            await ((IWritableSwitch)dlOutlet).SetValue();
 
-    [Fact]
-    public async Task Poll_ShouldReturnFalse_WhenResponseIsUnsuccessful() {
-        // Arrange
-        var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
-        handlerMock
-            .Protected()
-            .Setup<Task<HttpResponseMessage>>(
+            // Assert
+            handlerMock.Protected().Verify(
                 "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
+                Times.Once(),
+                ItExpr.Is<HttpRequestMessage>(req =>
+                    req.Method == HttpMethod.Put &&
+                    req.RequestUri == new Uri("http://localhost/restapi/relay/outlets/1/state/") &&
+                    req.Content.ReadAsStringAsync().Result == "value=true"
+                ),
                 ItExpr.IsAny<CancellationToken>()
-            )
-            .ReturnsAsync(new HttpResponseMessage {
-                StatusCode = HttpStatusCode.BadRequest,
-                Content = new StringContent("Bad Request")
-            })
-            .Verifiable();
+            );
+        }
 
-        var httpClient = new HttpClient(handlerMock.Object);
+        [Fact]
+        public async Task SetValue_ShouldLogError_WhenHttpRequestExceptionOccurs() {
+            // Arrange
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ThrowsAsync(new HttpRequestException("Request failed"))
+                .Verifiable();
 
-        var dlOutlet = new DlOutlet("TestOutlet", 1, httpClient, "localhost", "user", "password");
+            var httpClient = new HttpClient(handlerMock.Object);
 
-        // Act
-        var result = await dlOutlet.Poll();
+            var dlOutlet = new DlOutlet("TestOutlet", 1, httpClient, "localhost", "user", "password") {
+                TargetValue = 1d
+            };
 
-        // Assert
-        Assert.False(result);
-        Assert.Equal(0d, dlOutlet.Value);
-    }
+            // Act
+            await ((IWritableSwitch)dlOutlet).SetValue();
 
-    [Fact]
-    public async Task Poll_ShouldReturnFalse_WhenHttpRequestExceptionOccurs() {
-        // Arrange
-        var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
-        handlerMock
-            .Protected()
-            .Setup<Task<HttpResponseMessage>>(
+            // Assert
+            handlerMock.Protected().Verify(
                 "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
+                Times.Once(),
+                ItExpr.Is<HttpRequestMessage>(req =>
+                    req.Method == HttpMethod.Put &&
+                    req.RequestUri == new Uri("http://localhost/restapi/relay/outlets/1/state/") &&
+                    req.Content.ReadAsStringAsync().Result == "value=true"
+                ),
                 ItExpr.IsAny<CancellationToken>()
-            )
-            .ThrowsAsync(new HttpRequestException("Request failed"))
-            .Verifiable();
-
-        var httpClient = new HttpClient(handlerMock.Object);
-
-        var dlOutlet = new DlOutlet("TestOutlet", 1, httpClient, "localhost", "user", "password");
-
-        // Act
-        var result = await dlOutlet.Poll();
-
-        // Assert
-        Assert.False(result);
-        Assert.Equal(0d, dlOutlet.Value);
-    }
-
-    [Fact]
-    public async Task Poll_ShouldReturnFalse_WhenJsonExceptionOccurs() {
-        // Arrange
-        var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
-        handlerMock
-            .Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            )
-            .ReturnsAsync(new HttpResponseMessage {
-                StatusCode = HttpStatusCode.OK,
-                Content = new StringContent("invalid json")
-            })
-            .Verifiable();
-
-        var httpClient = new HttpClient(handlerMock.Object);
-
-        var dlOutlet = new DlOutlet("TestOutlet", 1, httpClient, "localhost", "user", "password");
-
-        // Act
-        var result = await dlOutlet.Poll();
-
-        // Assert
-        Assert.False(result);
-        Assert.Equal(0d, dlOutlet.Value);
-    }
-
-    [Fact]
-    public async Task SetValue_ShouldSetCorrectValue_WhenResponseIsSuccessful() {
-        // Arrange
-        var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
-        handlerMock
-            .Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            )
-            .ReturnsAsync(new HttpResponseMessage {
-                StatusCode = HttpStatusCode.NoContent
-            })
-            .Verifiable();
-
-        var httpClient = new HttpClient(handlerMock.Object);
-
-        var dlOutlet = new DlOutlet("TestOutlet", 1, httpClient, "localhost", "user", "password") {
-            TargetValue = 1d
-        };
-
-        // Act
-        await ((IWritableSwitch)dlOutlet).SetValue();
-
-        // Assert
-        handlerMock.Protected().Verify(
-            "SendAsync",
-            Times.Once(),
-            ItExpr.Is<HttpRequestMessage>(req =>
-                req.Method == HttpMethod.Put &&
-                req.RequestUri == new Uri("http://localhost/restapi/relay/outlets/1/state/") &&
-                req.Content.ReadAsStringAsync().Result == "value=true"
-            ),
-            ItExpr.IsAny<CancellationToken>()
-        );
-    }
-
-    [Fact]
-    public async Task SetValue_ShouldLogError_WhenResponseIsUnsuccessful() {
-        // Arrange
-        var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
-        handlerMock
-            .Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            )
-            .ReturnsAsync(new HttpResponseMessage {
-                StatusCode = HttpStatusCode.BadRequest,
-                Content = new StringContent("Bad Request")
-            })
-            .Verifiable();
-
-        var httpClient = new HttpClient(handlerMock.Object);
-
-        var dlOutlet = new DlOutlet("TestOutlet", 1, httpClient, "localhost", "user", "password") {
-            TargetValue = 1d
-        };
-
-        // Act
-        await ((IWritableSwitch)dlOutlet).SetValue();
-
-        // Assert
-        handlerMock.Protected().Verify(
-            "SendAsync",
-            Times.Once(),
-            ItExpr.Is<HttpRequestMessage>(req =>
-                req.Method == HttpMethod.Put &&
-                req.RequestUri == new Uri("http://localhost/restapi/relay/outlets/1/state/") &&
-                req.Content.ReadAsStringAsync().Result == "value=true"
-            ),
-            ItExpr.IsAny<CancellationToken>()
-        );
-    }
-
-    [Fact]
-    public async Task SetValue_ShouldLogError_WhenHttpRequestExceptionOccurs() {
-        // Arrange
-        var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
-        handlerMock
-            .Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            )
-            .ThrowsAsync(new HttpRequestException("Request failed"))
-            .Verifiable();
-
-        var httpClient = new HttpClient(handlerMock.Object);
-
-        var dlOutlet = new DlOutlet("TestOutlet", 1, httpClient, "localhost", "user", "password") {
-            TargetValue = 1d
-        };
-
-        // Act
-        await ((IWritableSwitch)dlOutlet).SetValue();
-
-        // Assert
-        handlerMock.Protected().Verify(
-            "SendAsync",
-            Times.Once(),
-            ItExpr.Is<HttpRequestMessage>(req =>
-                req.Method == HttpMethod.Put &&
-                req.RequestUri == new Uri("http://localhost/restapi/relay/outlets/1/state/") &&
-                req.Content.ReadAsStringAsync().Result == "value=true"
-            ),
-            ItExpr.IsAny<CancellationToken>()
-        );
+            );
+        }
     }
 }
