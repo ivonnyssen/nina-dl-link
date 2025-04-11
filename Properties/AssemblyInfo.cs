838be9659d624acb9d7ae0fffabd8b3b0274964e
Igor von Nyssen <igor@vonnyssen.com>
2025-04-10 22:54:10 -0700

    site and assemblyinfo updates

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -35,13 +35,13 @@ using System.Runtime.InteropServices;
 // The following attributes are optional for the official manifest meta data
 
 //[Optional] Your plugin homepage URL - omit if not applicaple
-[assembly: AssemblyMetadata("Homepage", "https://github.com/ivonnyssen/nina-dl-link/wiki")]
+[assembly: AssemblyMetadata("Homepage", "https://ivonnyssen.github.io/nina-dl-link/")]
 
 //[Optional] Common tags that quickly describe your plugin
 [assembly: AssemblyMetadata("Tags", "")]
 
 //[Optional] A link that will show a log of all changes in between your plugin's versions
-[assembly: AssemblyMetadata("ChangelogURL", "https://github.com/ivonnyssen/nina-dl-link/CHANGELOG.md")]
+[assembly: AssemblyMetadata("ChangelogURL", "https://github.com/ivonnyssen/nina-dl-link/blob/main/CHANGELOG.md")]
 
 //[Optional] The url to a featured logo that will be displayed in the plugin list next to the name
 [assembly: AssemblyMetadata("FeaturedImageURL", "https://ivonnyssen.github.io/nina-dl-link/assets/dl-logo.png")]
